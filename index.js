a894780176d7ea97d2aa5b083303ef4b05804bff
Enrique Alcantara <enrique@crunch.io>
2017-06-22 10:57:13 -0400

    add eslint react plugin

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ module.exports = {
             "jsx": true
         }
     },
+    "plugins": [
+        "react"
+    ],
+    "settings": {
+        "react": {
+            "createClass": "createReactClass", // Regex for Component Factory to use, default to "createReactClass"
+            "pragma": "React",  // Pragma to use, default to "React"
+            "version": "15.0" // React version, default to the latest React stable release
+        }
+    },
     // Any rule not explicitly defined here was, at the time of writing,
     // a default of the eslint recommended ruleset.
     "rules": {
